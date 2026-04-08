d4b02660bdabb942841877e81746fb01e10f114f
alonecoderpravee <praveen.shetty@nitte.edu.in>
2026-04-08 21:53:56 +0530

    IWP Lab Updated

--- MSE-SOLUTIONS/11.Inline.App.jsx
+++ MSE-SOLUTIONS/11.Inline.App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import myImage from './A.jpg';
 
 function App() {
-  const imageUrl = "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=500";
+  /* const myImage = "https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=500"; */
 
   return (
     <div style={{ padding: '40px', textAlign: 'center', backgroundColor: '#f4f4f9', minHeight: '100vh' }}>
@@ -17,7 +18,7 @@ function App() {
         boxShadow: '0 10px 30px rgba(0,0,0,0.2)' 
       }}>
         
-        <img src={imageUrl} alt="Tech" style={{ width: '100%', borderRadius: '10px', marginBottom: '15px' }} />
+        <img src={myImage} alt="Tech" style={{ width: '100%', borderRadius: '10px', marginBottom: '15px' }} />
         
         <h2 style={{ margin: '0', fontWeight: 'bold' }}>React Design</h2>
         <p style={{ color: '#dfe6e9', fontSize: '14px' }}>Inline styling is fast and easy!</p>
